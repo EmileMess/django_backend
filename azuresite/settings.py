afdc74a278c7e2cc9a57a46ae244a7e3989c0f50
Émile Messelken <97222315+EmileMess@users.noreply.github.com>
2022-03-03 21:45:25 +0100

    update

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
-    'polls.apps.PostConfig',
+    'post.apps.PostConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
