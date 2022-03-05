7fcc0ebb5c7f80910726649fb8f73038b1148a57
Émile Messelken <97222315+EmileMess@users.noreply.github.com>
2022-03-05 12:58:04 +0100

    remove polls

--- azuresite/urls.py
+++ azuresite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    # path('', include('polls.urls')),
     path('api/', include('post.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
