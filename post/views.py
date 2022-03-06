f88338f780299a4a2ea34c252d6441b239c8d265
Émile Messelken <97222315+EmileMess@users.noreply.github.com>
2022-03-06 14:48:43 +0100

    update

--- post/views.py
+++ post/views.py
@@ -41,8 +41,8 @@ class PostView(APIView):
     def post(self, request, *args, **kwargs):
         if request.method == "POST":
             allimages = request.FILES.getlist('images')
-            datasetname = request.get('datasetname')
-            useremail = request.get('user')
+            datasetname = request.FILES.get('datasetname')
+            useremail = request.FILES.get('user')
             Dataset.objects.create(name=datasetname, user=CustomUser.objects.get(email=useremail))
             for image in allimages:
                 Image.objects.create(name='img', image=image, dataset=Dataset.objects.get(name=datasetname, user=CustomUser.objects.get(email=useremail)))
