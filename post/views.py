872b5b40da1a124332ee96700c0059bfc741aaae
Émile Messelken <97222315+EmileMess@users.noreply.github.com>
2022-03-06 17:53:29 +0100

    update

--- post/views.py
+++ post/views.py
@@ -20,7 +20,7 @@ class PostView(APIView):
 
     def get(self, request, *args, **kwargs):
         image = Image.objects.all()
-        images = Image.objects.get(dataset=Dataset.objects.get(name="yyy", user=CustomUser.objects.get(email="e.mess1806@gmail.com")))
+        # images = Image.objects.get(dataset=Dataset.objects.get(name="yyy", user=CustomUser.objects.get(email="e.mess1806@gmail.com")))
         datasets = Dataset.objects.all()
         serializer = DatasetSerializer(datasets, many=True)
         return Response(serializer.data)
@@ -31,10 +31,15 @@ class PostView(APIView):
             datasetname = request.POST.get('datasetname')
             useremail = request.POST.get('user')
             user = CustomUser.objects.get(email=useremail)
+
+            # Check if dataset already exists
             if Dataset.objects.filter(name=datasetname, user=user).exists():
                 return Response(status=status.HTTP_403_FORBIDDEN)
-            Dataset.objects.create(name=datasetname, user=user)
+            
+            # Create new dataset and fill data
+            dataset = Dataset.objects.create(name=datasetname, user=user)
             for image in allimages:
-                Image.objects.create(name='img', image=image, dataset=Dataset.objects.get(name=datasetname, user=user))
+                Image.objects.create(name='img', image=image, dataset=dataset)
+
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
